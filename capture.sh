66642812cd980dfc507091d552ee2a4d00374b47
GIT Admin <jorges119@hotmail.com>
2015-10-26 14:00:56 +0000

    new Capture

--- capture.sh
+++ capture.sh
@@ -1,5 +1,5 @@
 rm imagefile.jpg
-/home/root/bin/ffmpeg/ffmpeg -ss 0.5 -i /dev/video0 -t 1 -s 1280x800 -f image2 -y -i imagefile.jpg
+/home/root/bin/ffmpeg/ffmpeg -ss 0.5 -i /dev/video0 -t 1 -s 1280x800 -f image2 -y imagefile.jpg
 
 git commit -a -m 'new Capture'
 git push -u origin master
